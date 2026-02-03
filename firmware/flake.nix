e5b88972e3629403cccb30b0a2a11de36548de80
Ivan Johnson <git@IvanJohnson.net>
2026-02-02 19:50:08 -0500

    WIP

--- firmware/flake.nix
+++ firmware/flake.nix
@@ -3,30 +3,24 @@
 
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-25.11-small";
-		rustnix = {
-			url = "github:nix-community/fenix";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
 	};
 
 	outputs =
 		{
 			self,
 			nixpkgs,
-			rustnix,
 		}:
 		let
 			pkgs = import nixpkgs { system = "x86_64-linux"; };
-			rustPlatform = pkgs.rustPlatform;
 		in
 		let
 			devshell = pkgs.mkShell {
 				buildInputs = [
-					(rustnix.packages.x86_64-linux.fromToolchainFile {
-						file = ./rust-toolchain.toml;
-						sha256 = "sha256-cQl292Ia+Crg9ps29Pv5ciufXd0b/HF7770/bOEDv+k=";
-					})
+					nixpkgs.espup
 				];
+  shellHook = ''
+    echo TODO run commands from SETUP.md?
+  '';
 			};
 
 		in
